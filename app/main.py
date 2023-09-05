9880b4fae0d33c549c8f285859457e01fd45ad8a
silenc3502 <gcccompil3r@gmail.com>
2023-09-06 01:48:45 +0900

    feat: Safe exit on reactive service [MF-9]

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from multiprocessing import Process
 from fastapi import FastAPI
 
@@ -21,7 +22,11 @@ app.include_router(socket_server_router)
 
 # uvicorn app.main:app --host 0.0.0.0 --port 8000
 if __name__ == '__main__':
-    socket_server_process = Process(target=run_socket_server, args=(fastapi_queue, socket_server_queue, ))
+    main_process_id = os.getpid()
+    print(f"현재 프로세스의 ID: {main_process_id}")
+
+    socket_server_process = Process(target=run_socket_server,
+                                    args=(fastapi_queue, socket_server_queue, main_process_id, ))
     socket_server_process.start()
     print("socket_server_process: ", socket_server_process)
 
--- app/socket_server/generator.py
+++ app/socket_server/generator.py
@@ -1,27 +1,120 @@
 import importlib
-from asyncio import sleep
+import multiprocessing
+import queue
+import signal
+from multiprocessing import Process
+from time import sleep
 
 socket_server_module = importlib.import_module("app.includes.Mercenary-Socket-Server.socket_server")
 server_instance = None
 
+import os
 
-def run_socket_server(fastapi_queue, socket_server_queue):
+
+# 사실 이 파트는 command_controller 라고 만드는 것이 더 좋았을 것이다
+def run_socket_server(fastapi_queue, socket_server_queue, main_process_id):
     print("run_socket_server")
-    server_instance = socket_server_module.SocketServer('0.0.0.0', 33333)
+    #server_instance = socket_server_module.SocketServer('0.0.0.0', 33333)
+    server_instance = None
+    main_socket_process = None
+
+    receiver_pid = None
+    transmitter_pid = None
+
+    transmitter = None
+
+    current_pid = os.getpid()
+    print("run_socket_server(command_controller) pid: ", current_pid)
+
+    pid_queue = multiprocessing.Queue()
 
     # 큐를 통해 FastAPI 애플리케이션과 통신
     while True:
-        command = fastapi_queue.get()
-        print("receive - command: ", command)
+        try:
+            command = fastapi_queue.get(block=False)
+            print("receive - command: ", command)
 
-        if command == 4:
-            server_instance.kill_all_process()
-            break
-        elif command == 1:
-            server_instance.start()
-        elif command == 333:
-            data_str = fastapi_queue.get()
-            transmitter = server_instance.get_transmitter()
-            transmitter.put_command_data(command, data_str)
+            if command == 4:
+                if main_socket_process and main_socket_process.is_alive():
+                    socket_pid = main_socket_process.pid
+                    print("socket pid: ", socket_pid)
 
-        sleep(0.5)
+                    server_instance = None
+
+                    try:
+                        os.kill(receiver_pid, signal.SIGTERM)
+                        print(f"SIGINT signal sent to receiver process {receiver_pid}.")
+
+                        os.kill(transmitter_pid, signal.SIGTERM)
+                        print(f"SIGINT signal sent to transmitter process {transmitter_pid}.")
+
+                        os.kill(socket_pid, signal.SIGTERM)
+                        print(f"SIGINT signal sent to socket process {socket_pid}.")
+
+                        #main_socket_process.terminate()
+
+                    except ProcessLookupError:
+                        print(f"PID {socket_pid} does not correspond to a running process.")
+                    except OSError as e:
+                        print(f"An error occurred while sending the signal: {e}")
+
+                    #main_socket_process.join()  # Wait for the process to complete (optional)
+                    #main_socket_process.terminate()
+                    main_socket_process = None
+                    transmitter = None
+
+                    print("All Clear")
+
+                    print("Finish to clean socket")
+                else:
+                    print("Socket server process is not running.")
+
+            elif command == 1:
+                if main_socket_process and main_socket_process.is_alive():
+                    # Stop the existing process gracefully
+                    main_socket_process.terminate()
+                    #main_socket_process.join()
+                    main_socket_process = None
+                    print("Stopped the existing Socket server process.")
+
+                server_instance = socket_server_module.SocketServer('0.0.0.0', 33333)
+                main_socket_process = Process(target=server_instance.start,
+                                              args=(fastapi_queue, socket_server_queue, pid_queue, ))
+                main_socket_process.start()
+
+                sleep(5)
+
+                pid_data = pid_queue.get()
+                print("pid_data: ", pid_data)
+
+                if pid_data[0] == 'rx':
+                    receiver_pid = pid_data[1]
+                else:
+                    transmitter_pid = pid_data[1]
+
+                pid_data = pid_queue.get()
+                print("pid_data: ", pid_data)
+
+                if pid_data[0] == 'rx':
+                    receiver_pid = pid_data[1]
+                else:
+                    transmitter_pid = pid_data[1]
+
+                print("tx pid: ", transmitter_pid)
+                print("rx pid: ", receiver_pid)
+
+                # server_instance = socket_server_module.SocketServer('0.0.0.0', 33333)
+                # main_socket_process = Process(target=server_instance.start, args=(fastapi_queue, socket_server_queue,))
+                # main_socket_process.start()
+
+            elif command == 333:
+                print("AI Command")
+                data_str = fastapi_queue.get()
+                transmitter = server_instance.get_transmitter()
+                transmitter.put_command_data(command, data_str)
+
+        except queue.Empty:
+            # 큐가 비어 있는 경우 예외 처리
+            #print("큐가 비어 있습니다.")
+            sleep(0.5)
+
